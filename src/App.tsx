016a7ed965cc942e694c9970cc50c80f0ada99c0
Matheus Malara <matheus_malara@live.com>
2025-09-27 20:29:46 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './styles/global.css'
+import './styles/theme.css'
+import {Container} from "./components/Container";
+import {Heading} from "./components/Heading";
+
+export function App() {
+    return (
+        <>
+            <Container>
+                <Heading>LOGO</Heading>
+            </Container>
+
+            <Container>
+                <Heading>MENU</Heading>
+            </Container>
+        </>
+    );
+}
